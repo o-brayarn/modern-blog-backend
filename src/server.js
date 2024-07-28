828a1aa82d5232979ffe7ea84c96fe3c0a79df1a
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-28 11:58:47 +0300

    create upvote update endpoint

--- src/server.js
+++ src/server.js
@@ -1,9 +1,32 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 // Defining different endpoints
-app.get("/hello", (req, res) => {
-  res.send("Hello There!");
+let articlesInfo = [
+  {
+    name: "learn-react",
+    upvotes: 0,
+  },
+  {
+    name: "learn-node",
+    upvotes: 0,
+  },
+  {
+    name: "mongodb",
+    upvotes: 0,
+  },
+];
+app.put("/api/articles/:name/upvotes", (req, res) => {
+  const { name } = req.params;
+  const article = articlesInfo.find((a) => a.name === name);
+
+  if (article) {
+    article.upvotes += 1;
+    res.send(`${name} now has ${article.upvotes} upvotes`);
+  } else {
+    res.send("This ${name} does not exist");
+  }
 });
 
 app.listen(8000, () => {
