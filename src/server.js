eb417ef97146dd7cb141a20ff072a792a2f6622b
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-29 07:20:55 +0300

    add Mongodb to Express

--- src/server.js
+++ src/server.js
@@ -1,28 +1,25 @@
 import express from "express";
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(express.json());
 // Defining different endpoints
-let articlesInfo = [
-  {
-    name: "learn-react",
-    upvotes: 0,
-    comments: [],
-  },
-  {
-    name: "learn-node",
-    upvotes: 0,
-    comments: [],
-  },
-  {
-    name: "mongodb",
-    upvotes: 0,
-    comments: [],
-  },
-];
 
-app.get("/api/articles", (req, res) => {
-  res.send(articlesInfo);
+app.get("/api/articles/:name", async (req, res) => {
+  const { name } = req.params;
+
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+
+  const db = client.db("react-blog-db");
+
+  const article = await db.collection("articles").findOne({ name });
+
+  if (article) {
+    res.json(article);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.put("/api/articles/:name/upvotes", (req, res) => {
